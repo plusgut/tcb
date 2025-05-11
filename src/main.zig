aef66fe2b6b9a0cbe364bd22b0d481a3ca65ab58
plusgut <github@webentwickler2-0.de>
2025-05-11 20:03:19 +0200

    feat(zig): add threading

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,60 @@
 const std = @import("std");
 
-pub fn main() !void{
-    const addr = try std.net.Address.parseIp("127.0.0.1", 4242);
-    var server: std.net.Server = try std.net.Address.listen(addr, .{ .reuse_address = true, .reuse_port = true });
-    var idx: usize = 0;
+var m = std.Thread.Mutex{};
 
-    std.debug.print("Server up and running !\n", .{});
+pub fn main() !void {
+  const addr = try std.net.Address.parseIp("127.0.0.1", 4242);
+  var server: std.net.Server = try std.net.Address.listen(addr, .{ .reuse_address = true, .reuse_port = true });
 
-    while (true) {
-      var buffer: [524288]u8 = undefined; // Buffer size does not affect performance
-      var conn = try server.accept(); // Blocking call
-      idx += 1;
-      defer conn.stream.close();
+  std.debug.print("Server up and running !\n", .{});
 
-      var http_server_with_client = std.http.Server.init(conn, &buffer);
+  // Thread pool
+  var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+  defer _ = gpa.deinit();
+  const allocator = gpa.allocator();
+  var pool: std.Thread.Pool = undefined;
+  try pool.init(.{ .allocator = allocator });
+  defer pool.deinit();
 
-      while (http_server_with_client.state == .ready) {
-          // Read request
-          var req = http_server_with_client.receiveHead() catch |err| switch (err) {
-              error.HttpConnectionClosing => break,
-              else => {
-                  std.debug.print("Unhandled error {any}", .{err});
-                  return;
-              },
-          };
-          _ = try req.reader();
-
-          // Send response
-          try req.respond("bonjour", .{});
-      }
-    }
+  while (true) {
+      handleClient(&server);
+      try pool.spawn(handleClient, .{&server});
+  }
 }
+
+// Accept new clients in the spawned thread
+fn handleClient(server: *std.net.Server) void {
+  const conn = server.*.accept() catch {
+      std.debug.print("Error while accepting a new client\n", .{});
+      return;
+  }; // Blocking call
+  var buffer: [1024]u8 = undefined; // Buffer size does not affect performance
+  var http_server_with_client = std.http.Server.init(conn, &buffer);
+  defer conn.stream.close();
+
+  // Simulate work
+  std.time.sleep(1 * std.time.ns_per_ms);
+
+  while (http_server_with_client.state == .ready) {
+      // Read request
+      var req = http_server_with_client.receiveHead() catch |err| switch (err) { // Blocking call
+          error.HttpConnectionClosing => break,
+          else => {
+              std.debug.print("Unhandled error {any}\n", .{err});
+              return;
+          },
+      };
+
+      _ = req.reader() catch |err| {
+          std.debug.print("Error while reading request: {any}\n", .{err});
+          return;
+      };
+
+      // Send response
+      req.respond("bonjour", .{}) catch |err| {
+          std.debug.print("Error while sending response: {any}\n", .{err});
+          return;
+      };
+  }
+}
+
